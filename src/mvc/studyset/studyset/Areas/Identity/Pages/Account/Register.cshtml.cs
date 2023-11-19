c8c71053dca91db65e9b48fca048a01b074e1a40
anabmilk <anabmilk@gmail.com>
2023-11-19 12:27:11 -0300

    integração front-end registro/login

--- src/mvc/studyset/studyset/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ src/mvc/studyset/studyset/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -74,19 +74,24 @@ namespace studyset.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Required]
+            /// 
+            [Required(ErrorMessage = "Obrigatório informar o nome")]
+            [Display(Name = "Nome")]
+            public string UserName { get; set; }
+
+            [Required(ErrorMessage = "Obrigatório informar o e-mail")]
             [EmailAddress]
-            [Display(Name = "Email")]
+            [Display(Name = "E-mail")]
             public string Email { get; set; }
 
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [Required(ErrorMessage = "Obrigatório informar a senha")]
+            [StringLength(100, ErrorMessage = "A {0} precisa ter no mínimo {2} e no máximo {1} caracteres.", MinimumLength = 6)]
             [DataType(DataType.Password)]
-            [Display(Name = "Password")]
+            [Display(Name = "Senha")]
             public string Password { get; set; }
 
             /// <summary>
@@ -94,8 +99,8 @@ namespace studyset.Areas.Identity.Pages.Account
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [DataType(DataType.Password)]
-            [Display(Name = "Confirm password")]
-            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            [Display(Name = "Confirmar senha")]
+            [Compare("Password", ErrorMessage = "Os campos de senha devem coincidir.")]
             public string ConfirmPassword { get; set; }
         }
 
