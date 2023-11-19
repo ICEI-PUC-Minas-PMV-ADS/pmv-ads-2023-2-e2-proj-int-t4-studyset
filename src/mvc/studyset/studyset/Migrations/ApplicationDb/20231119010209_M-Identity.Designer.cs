1413ecc1ba621ad3b8f4850aee8b18a6946497a8
anabmilk <anabmilk@gmail.com>
2023-11-18 22:10:31 -0300

    Configuração das tabelas do Identity

--- src/mvc/studyset/studyset/Migrations/ApplicationDb/20231119010209_M-Identity.Designer.cs
+++ src/mvc/studyset/studyset/Migrations/ApplicationDb/20231119010209_M-Identity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using studyset.Data;
 
@@ -11,9 +12,11 @@ using studyset.Data;
 namespace studyset.Migrations.ApplicationDb
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231119010209_M-Identity")]
+    partial class MIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
