1ccf6cd38c660ec5bdcfb4b6e79e5afc62ce3a72
IsraelsilvaD <130614485+IsraelsilvaD@users.noreply.github.com>
2023-11-29 20:52:02 -0300

    Testando cod

---
+++ src/mvc/studyset/studyset/Views/Agenda/agenda.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace studyset.Views.Agenda
+{
+    public class agendaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
